b43f315d2c657c5df8c0fd7b6aed594ba4cb69b2
gfktstv <g.feoktistoff@gmail.com>
2023-11-14 23:46:57 +0700

    code v1.1
    Created class system and add pdf file with description of class system (text in Russian + diagram in English):
    - Added classes Text, Paragraph, Sentence and Word to work with text
    - Added abstract class LexicalResourceStats with child class SemanticRelationsStats to evaluate LR criteria
    
    However, these classes are not completed -- only classes relations was created and work on LR criteria evaluating started:
    - Created attribute "list_of_all_words" in class LexicalResourceStats with list of all word from content
    - Created attribute "words_for_semantic_connections_evaluating" in class SemanticRelationsStats with list of words suitable to evaluate semantic relations

--- main.py
+++ main.py
@@ -1,63 +1,187 @@
-import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+
+import re
+
+from itertools import chain
 
 with open('wt2.txt', 'r') as file:
     text1 = file.read().replace('\n', '')
 
 
-class WT2:
-    def __init__(self, content):
-        self.content = content.lower()
+class Text:
+    """
+    Class to work with a whole text of an essay
 
-    def lr_evaluator(self):
-        #
-        # 1. Deleting punctuation marks
-        #
-        dict_of_punctuation_marks = {
-            '!': '', '.': '',  # ! and .
-            ',': '', ';': '',  # , and ;
-            '...': '', ':': '',  # ... and :
-            '(': '', ')': '',  # ()
-            '"': '', "'": '',  # ' and "
-            '-': '', '—': '',  # - and —
-            '?': '', '_': '',  # ? and _
-        }
+    Arguments:
+        source (str): Source of Data consisting a text and a question
 
-        # Create a pattern (regular expression) from the dictionary keys
-        pattern_from_dict = re.compile(
-            "(%s)" % "|".join(map(re.escape, dict_of_punctuation_marks.keys()))
-        )
+    Attributes:
+        content (str): The content of an essay
 
-        # For each match, look-up corresponding value in dictionary
-        self.content = pattern_from_dict.sub(
-            lambda m: dict_of_punctuation_marks[m.group()], self.content
-        )
-        #
-        # 2. Tokenization
-        #
-        self.content = nltk.word_tokenize(self.content)
-        #
-        # 3. Deleting stopwords
-        #
-        unique_words = []
-        for i in range(len(self.content)):
-            if self.content[i] not in stopwords.words('english'):
-                unique_words.append(self.content[i])
-        #
-        # 4. Deleting unnecessary parts of speech
-        #
-        unique_words_pos = nltk.pos_tag(unique_words)
+    """
 
+    def __init__(self, source):
+        self.source = source
+        with open(f'{self.source}', 'r') as file:
+            self.content = file.read().replace('\n', ' ')
+
+
+class Paragraph:
+    """
+    Class to work with a paragraph from the text
+    """
+
+
+class Sentence:
+    """
+    Class to work with a sentence
+    """
+
+
+class Word:
+    """
+    Class to get characteristics of a word
+
+    Description:
+        Class that shows several characteristics of a word
+        such as list of synonyms, list of lemmas,
+        count of repetition in the text and so on. All these
+        parameters are need to evaluate Lexical Resource of the text.
+
+    Arguments:
+        word_couple (list): word and part of speech
+
+    Attributes:
+        content (str): word itself
+        list_of_meanings (list): synset from wordnet
+        list_of_all_synonyms (list): all synonyms of the word
+
+    Methods:
+
+    """
+
+    def __init__(self, word_couple):
+        self.word_couple = word_couple
+        self.content = self.word_couple[0]
+        self.list_of_meanings = wn.synsets(f'{self.word_couple[0]}', pos=self.word_couple[1])
+
+        self.list_of_all_synonyms = []
+        for synset in self.list_of_meanings:
+            self.list_of_all_synonyms.append(
+                [str(lemma.name()) for lemma in synset.lemmas()]
+            )
+
+
+class LexicalResourceStats:
+    """
+    Abstract class to evaluate lexical resource criteria
+
+    Arguments:
+        text_class_variable (Text): text of an essay
+
+    Attributes:
+        content (str): The content of an essay
+        list_of_all_words (list): List of all words
+    """
+    pass
+
+    def __init__(self, text_class_variable):
+        self.content = text_class_variable.content
+        self.list_of_all_words = nltk.word_tokenize(self.content.lower())
+        self.list_of_all_words = nltk.pos_tag(self.list_of_all_words)
+
+        # delete punctuations marks
+        punctuation_marks_list = [
+            '!', '.', ',', ';', ':'
+                                '...', ')', '(', '"', '',
+            "'", '-', '--', '?', '—',
+            '[', ']'
+        ]
+
+        for word_tuple in self.list_of_all_words:
+            if word_tuple[0] in punctuation_marks_list:
+                self.list_of_all_words.remove(word_tuple)
+
+
+class SemanticRelationsStats(LexicalResourceStats):
+    """
+    Child class of LexicalResourceStats to evaluate semantic relations
+
+    Attributes:
+        intermediate_list_of_words (list): auxiliary attribute
+        words_for_semantic_connections_evaluating (list): list of words suitable for evaluating
+
+    Methods:
+        semantic_relations_algorithm_1: first algorithm to evaluate semantic relations
+    """
+
+    def __init__(self, text_class_variable):
+        super().__init__(text_class_variable)
+        #
+        # create a list of words that suitable for semantic relations evaluating
+        #
+        self.intermediate_list_of_words = self.list_of_all_words
+        self.intermediate_list_of_words = list(set(self.intermediate_list_of_words))
+
+        # delete stopwords
+        for word_couple in self.intermediate_list_of_words:
+            if word_couple[0] in stopwords.words('english'):
+                self.intermediate_list_of_words.remove(word_couple)
+
+        # delete unnecessary parts of speech
         necessary_pos_list = [
-            'JJ', 'JJR', 'JJS', 'NN', 'NNS', 'NNP', 'NNPS', 'VBZ',
+            'JJ', 'JJR', 'JJS', 'NN', 'NNS', 'VBZ',
             'VBP', 'VBN', 'VBD', 'VBG', 'VB', 'RBS', 'RB', 'RBR'
         ]
-        unique_words = []
-        for i in range(len(unique_words_pos)):
-            if unique_words_pos[i][1] in necessary_pos_list:
-                unique_words.append(unique_words_pos[i][0])
+        self.words_for_semantic_connections_evaluating = []
+        for word_couple in self.intermediate_list_of_words:
+            if word_couple[1] in necessary_pos_list:
+                self.words_for_semantic_connections_evaluating.append(word_couple)
 
+        # convert word_tuple into word_list (word_couple)
+        for i in range(len(self.words_for_semantic_connections_evaluating)):
+            self.words_for_semantic_connections_evaluating[i] = list(self.words_for_semantic_connections_evaluating[i])
 
-text_WT2 = WT2(text1)
-print(text_WT2.lr_evaluator())
+        # convert nltk pos_tags into wordnet pos_tags
+        noun_pattern = re.compile(
+            '^N.*'
+        )
+
+        verb_pattern = re.compile(
+            '^V.*'
+        )
+
+        adjective_pattern = re.compile(
+            '^J.*'
+        )
+
+        adverb_pattern = re.compile(
+            '^R.*'
+        )
+
+        for word_couple in self.words_for_semantic_connections_evaluating:
+            word_couple[1] = noun_pattern.sub(wn.NOUN, word_couple[1])
+            word_couple[1] = verb_pattern.sub(wn.VERB, word_couple[1])
+            word_couple[1] = adjective_pattern.sub(wn.ADJ, word_couple[1])
+            word_couple[1] = adverb_pattern.sub(wn.ADV, word_couple[1])
+
+        # create Word class objects
+        for i in range(len(self.words_for_semantic_connections_evaluating)):
+            self.words_for_semantic_connections_evaluating[i] = Word(self.words_for_semantic_connections_evaluating[i])
+
+    def semantic_relations_algorithm_1(self):
+        """
+        Method to evaluate semantic relations by first algorithm
+
+        Description:
+            Evaluating consists of a few steps:
+            1. Sort a list of words by a number of synonyms (synonyms for each value of word)
+            2. Starting from a first element of the list (this element will have maximum synonyms)
+               we check each next word if that word in the list of synonyms
+            2.1. If next word is in list of synonyms than we calculate this as 1 semantic relation
+            2.2. If next word is not in list of synonyms -- we do nothing
+            3. When we check each word we move to the second word and start cycle again
+        :return:
+        """
