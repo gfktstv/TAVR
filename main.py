a3a3bfe801e84bd1ff9ee510d081d8515339f94e
gfktstv <g.feoktistoff@gmail.com>
2024-01-02 19:04:08 +0700

    Refactored SemanticRelationsStats
    List of changes
    1. Instead of using semantic chains we started to use semantic lists to evaluate semantic relations
    2. Refactored SemanticRelationsStats
    3. Method get_semantic_relations_stats is not completed, we should change it to make it valid
    4. Added new class — Semantic List, which is used in class SemanticRelationsStats
    
    Description of new way to evaluate semantic relations:
    We tried to use different way to evaluate semantic relations, because there is no clear code in previous method. Subsequently, a new method was needed. There are two approaches: by semantic lists and by semantic spaces.
    
    The first way is easier, it is about creating a list of semantically related word for each word in the text. Then, by refining these lists to valuable lists we may calculate the average relationship between amount of unique semants and amount of their usage in a text.
    
    The second is harder, it is about extracting from a text a list of words that semantically connected with each other. It is more looks like semantic space.
    
    Description of validity problem:
    After completing refining semantic lists by the code it was expected to give different number of the average relationship between low- and high-scored essays, but it did not. It may be caused by semantic lists with the low relationship because these lists are usually short (2~ words in the list except hypersemant or word that construct the list).

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import nltk
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet as wn
-from nltk.stem.snowball import SnowballStemmer
-
 import spacy
-from spacy import displacy
 import en_core_web_lg
 
 from enchant.checker import SpellChecker
-import enchant
 
-import re
-
-import itertools
-from itertools import chain
+from collections import Counter
 
 with open('wt2.txt', 'r') as file:
     text1 = file.read().replace('\n', '')
@@ -43,53 +33,6 @@ class Text:
 
     def __str__(self):
         return text.content
-
-
-# possible that I will not use class Word because I have spaCy tokens with all features I need
-class Word:
-    """
-    Class to get characteristics of a word
-
-    Description:
-        Class that shows several characteristics of a word
-        such as list of synonyms, list of lemmas,
-        count of repetition in the text and so on. All these
-        parameters are need to evaluate Lexical Resource of the text.
-
-    Arguments:
-        word_couple (list): word and part of speech
-
-    Attributes:
-        content (str): word itself
-        content_stemmed (str): word stem (without affixes)
-        part_of_speech (str): part of speech of word
-        list_of_meanings (list): synset from wordnet
-        list_of_all_synonyms (list): all synonyms of the word
-
-    Methods:
-
-    """
-
-    def __init__(self, word_couple):
-        self.word_couple = word_couple
-        self.content = self.word_couple[0]
-        self.content_stemmed = SnowballStemmer('english').stem(self.content)
-
-        self.part_of_speech = self.word_couple[1]
-
-        self.list_of_meanings = wn.synsets(f'{self.word_couple[0]}', pos=self.word_couple[1])
-
-        self.list_of_all_synonyms = []
-        for synset in self.list_of_meanings:
-            self.list_of_all_synonyms.append(
-                [str(lemma.name()) for lemma in synset.lemmas()]
-            )
-        self.list_of_all_synonyms = list(chain.from_iterable(self.list_of_all_synonyms))
-
-        self.count_of_occurrences = 0
-
-    def get_count_of_occurrences(self, list_of_stems):
-        self.count_of_occurrences = list_of_stems.count(self.content_stemmed)
 
 
 class LexicalResourceStats:
@@ -141,12 +84,9 @@ class SemanticRelationsStats(LexicalResourceStats):
     Child class of LexicalResourceStats to evaluate semantic relations
 
     Attributes:
-        semantic_field (set): Set of words that semantically related to the question
-        unique_tokens (set): Unique token from the content
-
-        pos_equal_words_pairs (list): Pairs of words which lemmas are not equal and pos are equal
-        semantic_related_pairs (list): Pairs of words that have semantic connection
-        semantic_chains (list): Chain of semantic related words
+        tokens (set): Valuable tokens from the content (without puncts and stop words)
+        sem_list (list): Paradigm of semantic related words to the one
+        sem_lists (list): List of semantic lists
 
     Methods:
         get_statistics_of_semantic_relations
@@ -154,63 +94,126 @@ class SemanticRelationsStats(LexicalResourceStats):
 
     def __init__(self, text_class_variable):
         super().__init__(text_class_variable)
-        self.semantic_field = set()
+        self.content = text_class_variable.content
+        self.tokens = list()
+        self.sem_list = list()
+        self.sem_lists = list()
 
-        self.unique_tokens = set()
-        for token in self.words_without_stopwords_and_punct_marks:
-            self.unique_tokens.add(token)
-
-        # for get_statistics_of_semantic_relations
-        self.pos_equal_words_pairs = list()
-        self.semantic_related_pairs = list()
-        self.semantic_chains = list()
-
-    def get_statistics_of_semantic_relations(self):
+    def get_semantic_relations_stats(self):
         """
-                Method to evaluate semantic relations by first algorithm
+        This algorithm extracts from text semantic lists - paradigm of words which semantically connected to the one.
 
-                Description of algorithm:
-                    1. Create a set of pairs of all unique words
-                    2. We take each pair and check 2 parameters: similarity (nltk) and amount of values
-                        2.1 We consider pairs of words as semantic relational when similarity is more than
-                        2.2 We assign a weight (.25, .5, 1) of semantic relation by amount of values of words in pair
-                :return:
+        Description of the algorithm:
+        1. Extract valuable tokens from the content.
+        2. For each token search semantically related words in the text (>0.65 similarity, but <0.8)
+        3. Add each semantic list to a list of semantic lists
+        4. Clear semantic lists:
+        4.1. Delete lists that are part of other lists (sublists)
+        4.2. Delete lists which contain 1 element with 1 usage
         """
+        self.tokens = [
+            token for token in self.nlp_doc
+            if not token.is_stop and not token.is_punct
+        ]
 
-        # create a list with pairs of words which lemmas are not equal and pos are equal
-        self.pos_equal_words_pairs = list(
-            [x, y] for x in self.unique_tokens for y in self.unique_tokens if x.lemma_ != y.lemma_ if x.pos_ == y.pos_
-        )
+        # Delete duplicates
+        unique_tokens_text = []
+        unique_tokens = []
+        for token in self.tokens:
+            if token.text not in unique_tokens_text:
+                unique_tokens.append(token)
+                unique_tokens_text.append(token.text)
 
-        # create a list with semantic related pairs of words
-        for pair in self.pos_equal_words_pairs:
-            if pair[0].similarity(pair[1]) > 0.7:
-                self.semantic_related_pairs.append(pair)
+        self.tokens = unique_tokens
 
-        # create a list with semantic chains
-        # semantic chain is all semantically related words from the content
-        for i in range(len(self.semantic_related_pairs)):
-            semantic_chain = [self.semantic_related_pairs[i]]
-            for j in range(i + 1, len(self.semantic_related_pairs)):
-                if len(list(set(self.semantic_related_pairs[i]).intersection(set(self.semantic_related_pairs[j])))) > 0:
-                    semantic_chain.append(self.semantic_related_pairs[j])
-            self.semantic_chains.append(
-                list(set(chain.from_iterable(semantic_chain)))
-            )
+        # Create semantic lists
+        for hypersemant in self.tokens:
+            self.sem_list = list()
+            for token in self.tokens:
+                # We add tokens by their similarity parameter with similar pos tag except the similar tokens
+                if (hypersemant.similarity(token) > 0.6 and (hypersemant.similarity(token)) < 0.8 \
+                        and hypersemant.lemma_ != token.lemma_ \
+                        and hypersemant.pos_ == token.pos_ and token not in self.sem_list):
+                    self.sem_list.append(token)
+            self.sem_list = SemanticList(hypersemant, self.sem_list, self.tokens)
+            if self.sem_list.len > 0 and self.sem_list not in self.sem_lists:
+                self.sem_lists.append(self.sem_list)
 
-        print(self.semantic_chains)
+        # An algorithm to delete sublists:
+        # 1. Compare only 2+ elements lists
+        # 2. If an i-list is a sublist of a j-list and they are not the same lists we add +1 to a count variable
+        # 3. If after checking all j-lists the count is 0 --> the algorithm adds i-list to the temp_sem_lists
 
-        self.semantic_chains = list()
-        semantic_chain = set()
-        self.unique_tokens = list(self.unique_tokens)
-        for i in range(0, 5):
-            semantic_chain = set(
-                x.lemma_ for x in self.unique_tokens if self.unique_tokens[i].similarity(x) > 0.7
-            )
-            semantic_chain.add(self.unique_tokens[i].lemma_)
-            self.semantic_chains.append(semantic_chain)
-            self.unique_tokens = list(
-                set(self.unique_tokens) - semantic_chain
-            )
+        temp_sem_lists = []
+        for i in range(len(self.sem_lists)):
+            count = 0
+            for j in range(len(self.sem_lists)):
+                if self.sem_lists[i].lemmas.intersection(self.sem_lists[j].lemmas) == self.sem_lists[i].lemmas and i!=j:
+                    count += 1
+            if count == 0 and self.sem_lists[i].len > 2:
+                temp_sem_lists.append(self.sem_lists[i])
 
-        return self.semantic_chains
+        self.sem_lists = temp_sem_lists
+
+        return self.sem_lists
+
+
+class SemanticList:
+    """
+    This class is used to evaluate semantic relations by special property which is the relationship between
+    the unique lemmas of the list and the number of uses of tokens in the text
+
+    Arguments:
+        hypersemant (spaCy token): A token by which similarity semantic list is constructed
+        sem_list (list): A list contains semantically related tokens to the hypersemant
+        nlp_doc (spaCy doc): Doc object which contains tokens from the essay
+
+    Attributes:
+        hypersemant (spaCy token): A token by which similarity semantic list is constructed
+        list (list): A list contains semantically related tokens to the hypersemant
+        lemmas (list): A list contains lemmas of the tokens
+        len (int): The length of the list
+        list_text (list): A list contains text of the tokens
+        nlp_doc (spaCy doc): A doc object which contains tokens from the essay
+    """
+    def __init__(self, hypersemant, sem_list, nlp_doc):
+        self.hypersemant = hypersemant
+        self.list = sem_list
+        self.nlp_doc = nlp_doc
+
+        # Delete duplicates
+        unique_tokens = list()
+        self.lemmas = list()
+        for token in self.list:
+            if token.lemma_ not in self.lemmas:
+                unique_tokens.append(token)
+                self.lemmas.append(token.lemma_)
+        self.list = unique_tokens
+        self.lemmas = set(self.lemmas)
+
+        self.list_text = [token.text for token in unique_tokens]
+        self.len = len(self.lemmas)
+
+        # Calculate average similarity of the list by dividing sum of similarities by self.len
+        if self.len > 0:
+            self.avg_similarity = sum([self.hypersemant.similarity(token) for token in self.list]) / self.len
+
+        # Calculate the relationship between the unique lemmas of the list
+        # and the number of uses of tokens in the text.
+        # An algorithm is:
+        # 1. Assume Counter of tokens' texts from a text to a count variable.
+        # 2. Calculate number of occurrences for each token and add it to count_of_occurrences.
+        # 3. Divide self.len by count_of_occurrences.
+        tokens = [
+            token.text for token in self.nlp_doc
+            if not token.is_stop and not token.is_punct
+        ]
+        count = Counter(tokens)
+        count_of_occurrences = sum([count[token] for token in tokens])
+        self.lex_div_characteristic = self.len / count_of_occurrences
+
+
+text = Text('wt2.txt')
+test = SemanticRelationsStats(text)
+for semantic_list in test.get_semantic_relations_stats():
+    print(semantic_list.list, semantic_list.lex_div_characteristic, semantic_list.avg_similarity)
