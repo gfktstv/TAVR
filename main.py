ffc8d15fe216779c8ec062647e358b24709b2ecb
gfktstv <g.feoktistoff@gmail.com>
2024-03-24 01:02:10 +0700

    Added class structure.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import spacy
 import en_core_web_lg
 
-from enchant.checker import SpellChecker
-
 from collections import Counter
-
-with open('wt2.txt', 'r') as file:
-    text1 = file.read().replace('\n', '')
 
 nlp = en_core_web_lg.load()
 
@@ -21,6 +15,14 @@ class Text:
     Attributes:
         content (str): Content of the essay
         nlp_doc (spaCy doc): Doc object which contains tokens from the essay
+        tokens (list): All tokens from the text (without punctuation's marks)
+        tokens_without_stop (list): Tokens variable without stop words
+        unique_tokens_texts (list): Tokens without token.text duplicates
+        unique_tokens_lemmas (list): Tokens without token.lemma duplicates
+
+    Methods:
+        mistakes_correction
+        mistakes_statistics
 
     """
 
@@ -30,101 +32,102 @@ class Text:
             self.content = file.read().replace('\n', ' ')
 
         self.nlp_doc = nlp(self.content)
+        self.tokens = [token for token in self.nlp_doc if not token.is_punct]
+        self.tokens_without_stop = [token for token in self.nlp_doc if not token.is_stop]
+
+        self.unique_tokens_texts = []
+        unique_tokens_text = []
+        for token in self.tokens:
+            if token.text not in unique_tokens_text:
+                self.unique_tokens_texts.append(token)
+                unique_tokens_text.append(token.text)
+
+        self.unique_tokens_lemmas = []
+        unique_tokens_lemmas = []
+        for token in self.tokens:
+            if token.lemma_ not in unique_tokens_lemmas:
+                self.unique_tokens_lemmas.append(token)
+                unique_tokens_lemmas.append(token.lemma_)
 
     def __str__(self):
         return text.content
 
 
-class LexicalResourceStats:
+class CorpusDrivenMethods:
     """
-    Abstract class to evaluate lexical resource criteria
-
-    Arguments:
-        text_class_variable (Text): Text class variable containing the content and the doc object
+    Class which evaluates vocabulary richness of the text by using corpus driven methods.
+    It contains several methods such as word frequency, N-gram statistics, word range,
+    word specificity, vocabulary CEFR level and academic vocabulary percentage.
+    All of them return statistics organized by dictionaries. At the end, statistics
+    method returns concatenated statistics' dictionaries from the methods.
 
     Attributes:
-        content (str): Content of the essay
-        nlp_doc (spaCy doc): Doc object which contains tokens from the essay
-        words_without_stopwords_and_punct_marks (list): List of words without stopwords and punctuation marks
-    """
 
-    def __init__(self, text_class_variable):
-        self.content = text_class_variable.content
-        self.nlp_doc = text_class_variable.nlp_doc
-
-        self.words_without_stopwords_and_punct_marks = []
-        for token in self.nlp_doc:
-            if not token.is_stop and not token.is_punct:
-                self.words_without_stopwords_and_punct_marks.append(token)
-
-
-class WordSpellingAndFormationStats(LexicalResourceStats):
-    """
-    Child class of LexicalResourceStats to check word spelling and formation mistakes
-
-    Attributes:
     Methods:
+        word_frequency_statistics
+        N_gram_statistics
+        word_range_statistics
+        word_specificity_statistics
+        vocabulary_by_CEFR_level_statistics
+        academic_vocabulary_statistics
+        statistics
+
     """
 
-    def __init__(self, text_class_variable):
-        super().__init__(text_class_variable)
-        self.spelling_checker = SpellChecker('en')
-        self.spelling_checker.set_text(self.content)
-        self.errors_counter = 0
-
-    def get_word_spelling_and_formation_stats(self):
-        for err in self.spelling_checker:
-            self.errors_counter += 1
-
-        return self.errors_counter
+    def __init__(self):
+        pass
 
 
-class SemanticRelationsStats(LexicalResourceStats):
+class MTLD:
     """
-    Child class of LexicalResourceStats to evaluate semantic relations
+    Class MTLD evaluates lexical diversity (part of vocabulary richness) using MTLD index.
+    Returns a dictionary with statistics
 
     Attributes:
-        tokens (set): Valuable tokens from the content (without puncts and stop words)
+
+    Methods:
+        statistics
+
+    """
+
+    def __init__(self):
+        pass
+
+
+class SemanticRelations:
+    """
+    Class which evaluates vocabulary richness of the text on semantic relations.
+    It uses an algorithm which create a paradigm of semantically related words
+    from the text for each token and then extract indicators of those semantic lists.
+    Returns a dictionary with statistics
+
+    Attributes:
+        tokens (set): Tokens without token.text duplicates
         sem_list (list): Paradigm of semantic related words to the one
         sem_lists (list): List of semantic lists
 
     Methods:
-        get_statistics_of_semantic_relations
+        statistics
+
     """
 
     def __init__(self, text_class_variable):
-        super().__init__(text_class_variable)
         self.content = text_class_variable.content
-        self.tokens = list()
+        self.tokens = text_class_variable.unique_tokens_texts
         self.sem_list = list()
         self.sem_lists = list()
 
-    def get_semantic_relations_stats(self):
+    def statistics(self):
         """
         This algorithm extracts from text semantic lists - paradigm of words which semantically connected to the one.
 
         Description of the algorithm:
-        1. Extract valuable tokens from the content.
-        2. For each token search semantically related words in the text (>0.65 similarity, but <0.8)
-        3. Add each semantic list to a list of semantic lists
-        4. Clear semantic lists:
-        4.1. Delete lists that are part of other lists (sublists)
-        4.2. Delete lists which contain 1 element with 1 usage
+        1. For each token search semantically related words in the text (>0.65 similarity, but <0.8)
+        2. Add each semantic list to a list of semantic lists
+        3. Clear semantic lists:
+        3.1. Delete lists that are part of other lists (sublists)
+        3.2. Delete lists which contain 1 element with 1 usage
         """
-        self.tokens = [
-            token for token in self.nlp_doc
-            if not token.is_stop and not token.is_punct
-        ]
-
-        # Delete duplicates
-        unique_tokens_text = []
-        unique_tokens = []
-        for token in self.tokens:
-            if token.text not in unique_tokens_text:
-                unique_tokens.append(token)
-                unique_tokens_text.append(token.text)
-
-        self.tokens = unique_tokens
 
         # Create semantic lists
         for hypersemant in self.tokens:
@@ -150,12 +153,29 @@ class SemanticRelationsStats(LexicalResourceStats):
             for j in range(len(self.sem_lists)):
                 if self.sem_lists[i].lemmas.intersection(self.sem_lists[j].lemmas) == self.sem_lists[i].lemmas and i!=j:
                     count += 1
-            if count == 0 and self.sem_lists[i].len > 2:
+            if count == 0 and self.sem_lists[i].len > 0:
                 temp_sem_lists.append(self.sem_lists[i])
 
         self.sem_lists = temp_sem_lists
 
-        return self.sem_lists
+        # Output
+        LexDivCharacteristicCount = 0
+        AverageSimilarityCount = 0
+        LenCount = 0
+        Count = 0
+        for semantic_list in self.sem_lists:
+            LexDivCharacteristicCount += semantic_list.lex_div_characteristic
+            AverageSimilarityCount += semantic_list.avg_similarity
+            LenCount += semantic_list.len
+            Count += 1
+
+        statistics_dict = {
+            'LexDivCharacteristic average': LexDivCharacteristicCount / Count,
+            'AverageSimilarity average': AverageSimilarityCount / Count,
+            'Length average': LenCount / Count
+        }
+
+        return statistics_dict
 
 
 class SemanticList:
@@ -175,11 +195,12 @@ class SemanticList:
         len (int): The length of the list
         list_text (list): A list contains text of the tokens
         nlp_doc (spaCy doc): A doc object which contains tokens from the essay
+
     """
-    def __init__(self, hypersemant, sem_list, nlp_doc):
+    def __init__(self, hypersemant, sem_list, tokens):
         self.hypersemant = hypersemant
         self.list = sem_list
-        self.nlp_doc = nlp_doc
+        self.tokens = tokens
 
         # Delete duplicates
         unique_tokens = list()
@@ -204,16 +225,11 @@ class SemanticList:
         # 1. Assume Counter of tokens' texts from a text to a count variable.
         # 2. Calculate number of occurrences for each token and add it to count_of_occurrences.
         # 3. Divide self.len by count_of_occurrences.
-        tokens = [
-            token.text for token in self.nlp_doc
-            if not token.is_stop and not token.is_punct
-        ]
         count = Counter(tokens)
-        count_of_occurrences = sum([count[token] for token in tokens])
+        count_of_occurrences = sum([count[token] for token in self.tokens])
         self.lex_div_characteristic = self.len / count_of_occurrences
 
 
 text = Text('wt2.txt')
-test = SemanticRelationsStats(text)
-for semantic_list in test.get_semantic_relations_stats():
-    print(semantic_list.list, semantic_list.lex_div_characteristic, semantic_list.avg_similarity)
+test = SemanticRelations(text)
+print(test.statistics())
