4f30a2c93a17029e90a9f00bae7b70ef790244f9
gfktstv <g.feoktistoff@gmail.com>
2024-03-30 00:04:18 +0700

    MTLD class completed. Added word_freq_range, n_gram_freq_range, academic_vocabulary methods to the CorpusDrivenMethods class. Added brown single and tagged with part of speech brown single corpora.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import en_core_web_lg
+import spacy
+from spacy_ngram import NgramComponent
+
+from lexical_diversity import lex_div as ld
+from corpus_toolkit import corpus_tools as ct
 
 from collections import Counter
 
-nlp = en_core_web_lg.load()
+from math import log
+from statistics import mean
+
+nlp = spacy.load('en_core_web_lg')
+nlp.add_pipe('spacy-ngram')  # pipeline for n-gram marking
 
 
 class Text:
@@ -16,9 +24,12 @@ class Text:
         content (str): Content of the essay
         nlp_doc (spaCy doc): Doc object which contains tokens from the essay
         tokens (list): All tokens from the text (without punctuation's marks)
-        tokens_without_stop (list): Tokens variable without stop words
+        tokens_without_stop (list): Tokens without stop words
+        lemmas_without_stop (list): Lemmas without stop words
         unique_tokens_texts (list): Tokens without token.text duplicates
         unique_tokens_lemmas (list): Tokens without token.lemma duplicates
+        bigrams (list): 2-grams based on tokens
+        trigrams (list): 3-grams based on tokens
 
     Methods:
         mistakes_correction
@@ -33,21 +44,31 @@ class Text:
 
         self.nlp_doc = nlp(self.content)
         self.tokens = [token for token in self.nlp_doc if not token.is_punct]
-        self.tokens_without_stop = [token for token in self.nlp_doc if not token.is_stop]
+        self.tokens_without_stop = [token for token in self.tokens if not token.is_stop]
+        self.lemmas_without_stop = [token.lemma_ for token in self.tokens_without_stop]
 
-        self.unique_tokens_texts = []
-        unique_tokens_text = []
+        self.unique_tokens_texts = list()
+        unique_tokens_text = list()
         for token in self.tokens:
             if token.text not in unique_tokens_text:
                 self.unique_tokens_texts.append(token)
                 unique_tokens_text.append(token.text)
 
-        self.unique_tokens_lemmas = []
-        unique_tokens_lemmas = []
+        self.unique_tokens_lemmas = list()
+        unique_tokens_lemmas = list()
         for token in self.tokens:
             if token.lemma_ not in unique_tokens_lemmas:
                 self.unique_tokens_lemmas.append(token)
                 unique_tokens_lemmas.append(token.lemma_)
+
+        self.bigrams = list()
+        for i in range(len(self.tokens)-2):
+            self.bigrams.append(f'{self.tokens[i]}__{self.tokens[i+1]}'.lower())
+            self.bigrams.append(f'{self.tokens[i]}__{self.tokens[i+2]}'.lower())
+
+        self.trigrams = list()
+        for i in range(len(self.tokens)-2):
+            self.trigrams.append(f'{self.tokens[i]}__{self.tokens[i+1]}__{self.tokens[i+2]}'.lower())
 
     def __str__(self):
         return text.content
@@ -64,18 +85,152 @@ class CorpusDrivenMethods:
     Attributes:
 
     Methods:
-        word_frequency_statistics
-        N_gram_statistics
-        word_range_statistics
-        word_specificity_statistics
-        vocabulary_by_CEFR_level_statistics
-        academic_vocabulary_statistics
+        word_freq_range
+        n_gram_freq_range
+        word_specificity
+        vocabulary_by_level
+        academic_vocabulary
         statistics
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, text_class_variable):
+        self.nlp_doc = text_class_variable.nlp_doc
+        self.content = text_class_variable.content
+        self.tokens = text_class_variable.tokens
+        self.tokens_without_stop = text_class_variable.tokens_without_stop
+        self.lemmas_without_stop = text_class_variable.lemmas_without_stop
+        self.bigrams = text_class_variable.bigrams
+        self.trigrams = text_class_variable.trigrams
+
+        # Load and read tagged corpus, then tokenize (and lemmatize) it and create a frequency and range dictionary
+        self.brown_freq = ct.frequency(ct.tokenize(ct.ldcorpus('tagged_brown', verbose=False)))
+        self.brown_range = ct.frequency(ct.tokenize(ct.ldcorpus('tagged_brown', verbose=False)), calc='range')
+
+        # Create n-grams frequency and range dictionary
+        self.bigram_freq = ct.frequency(ct.tokenize(ct.ldcorpus('brown', verbose=False), lemma=False, ngram=2))
+        self.bigram_range = ct.frequency(ct.tokenize(ct.ldcorpus("brown", verbose=False), lemma=False, ngram=2), calc='range')
+        self.trigram_freq = ct.frequency(ct.tokenize(ct.ldcorpus('brown', verbose=False), lemma=False, ngram=3))
+        self.trigram_range = ct.frequency(ct.tokenize(ct.ldcorpus("brown", verbose=False), lemma=False, ngram=3), calc='range')
+
+        # List of tokens marked with frequency and range (including logarithmic scores)
+        self.tokens_freq_range = list()
+
+        # List of bigrams and trigrams marked with frequency and range (including logarithmic scores)
+        self.bigrams_freq_range = list()
+        self.trigrams_freq_range = list()
+
+        # Academic Word List
+        self.academic_word_list = list()
+        with open('academic word list/AWL.txt', 'r') as file:
+            for row in file:
+                self.academic_word_list.append(row.rstrip('\n'))
+
+    def word_freq_range(self):
+        """
+        Create a list with word and its frequency & range.
+        Calculates average word frequency & range (including logarithmic scores).
+
+        Returns:
+            1) list of tokens marked with frequency and range,
+            2) dict containing average frequency and range.
+        """
+        # Create lists of frequencies and ranges for calculating average values further
+        word_frequencies, word_ranges = list(), list()
+        for token in self.tokens:
+            if not token.is_stop:
+                try:
+                    word = f'{token.lemma_}_{token.pos_}'.lower()
+                    word_freq = self.brown_freq[word]
+                    word_range = self.brown_range[word]
+                    self.tokens_freq_range.append([token, f'{token.pos_}', word_freq, word_range])
+
+                    word_frequencies.append(word_freq)
+                    word_ranges.append(word_range)
+                except KeyError:
+                    self.tokens_freq_range.append([token, 'not_found'])
+            elif token.is_stop:
+                self.tokens_freq_range.append([token, 'stopword'])
+
+        statistics_dict = {
+            'Word frequency average: ': mean(word_frequencies),
+            'Word range average: ': mean(word_ranges),
+            'Word logarithmic frequency average: ': mean(log(x) for x in word_frequencies),
+            'Word logarithmic range average: ': mean(log(x) for x in word_frequencies)
+        }
+
+        return self.tokens_freq_range, statistics_dict
+
+    def n_gram_freq_range(self):
+        """
+        Create lists with bi-, trigrams and its frequency & range.
+        Calculates average bi-, trigrams frequency & range (including logarithmic scores).
+
+        Returns:
+            1) list of bigrams marked with frequency and range,
+            2) list of bigrams marked with frequency and range,
+            3) dict containing average frequency and range (including logarithmic scores).
+        """
+        # Create lists of frequencies and ranges for calculating average values further
+        bigram_frequencies, bigram_ranges = list(), list()
+        trigram_frequencies, trigram_ranges = list(), list()
+        for bigram in self.bigrams:
+            try:
+                bigram_freq = self.bigram_freq[bigram]
+                bigram_range = self.bigram_range[bigram]
+
+                bigram_frequencies.append(bigram_freq)
+                bigram_ranges.append(bigram_range)
+
+                # Append list of bigram and its frequency & range for marking content with high frequency bigrams
+                self.bigrams_freq_range.append([bigram, bigram_freq, bigram_range])
+            except KeyError:
+                self.bigrams_freq_range.append([bigram, 'not_found'])
+        for trigram in self.trigrams:
+            try:
+                trigram_freq = self.trigram_freq[trigram]
+                trigram_range = self.trigram_range[trigram]
+
+                trigram_frequencies.append(trigram_freq)
+                trigram_ranges.append(trigram_range)
+
+                # Append list of trigram and its frequency & range for marking content with high frequency trigrams
+                self.trigrams_freq_range.append([trigram, trigram_freq, trigram_freq])
+            except KeyError:
+                self.trigrams_freq_range.append([trigram, 'not_found'])
+
+        statistics_dict = {
+            'Bigram frequency average': mean(bigram_frequencies),
+            'Bigram range average': mean(bigram_ranges),
+            'Bigram logarithmic frequency average': mean(log(x) for x in bigram_frequencies),
+            'Bigram logarithmic range average': mean(log(x) for x in bigram_ranges),
+
+            'Trigram frequency average': mean(trigram_frequencies),
+            'Trigram range average': mean(trigram_ranges),
+            'Trigram logarithmic frequency average': mean(log(x) for x in trigram_frequencies),
+            'Trigram logarithmic range average': mean(log(x) for x in trigram_ranges)
+        }
+
+        return self.bigrams_freq_range, self.trigrams_freq_range, statistics_dict
+
+    def academic_words(self):
+        """
+        Calculates amount of academic words (words from Academic Word List) and the ratio
+        between the number of academic words and the number of tokens (not counting stopwords).
+
+        Returns: dictionary with amount of academic words and their percentage of the text
+        """
+        academic_words = list()
+        for token in self.tokens_without_stop:
+            if token.text in self.academic_word_list:
+                academic_words.append(token)
+
+        statistics_dict = {
+            'Amount of academic words': len(academic_words),
+            'Percentage of academic words': len(academic_words) / len(self.tokens_without_stop)
+        }
+
+        return statistics_dict
 
 
 class MTLD:
@@ -90,8 +245,11 @@ class MTLD:
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, text_class_variable):
+        self.flemmatized_text = ld.flemmatize(text_class_variable.content)
+
+    def statistics(self):
+        return {'MTLD': ld.mtld(self.flemmatized_text)}
 
 
 class SemanticRelations:
@@ -143,15 +301,15 @@ class SemanticRelations:
                 self.sem_lists.append(self.sem_list)
 
         # An algorithm to delete sublists:
-        # 1. Compare only 2+ elements lists
-        # 2. If an i-list is a sublist of a j-list and they are not the same lists we add +1 to a count variable
-        # 3. If after checking all j-lists the count is 0 --> the algorithm adds i-list to the temp_sem_lists
+        # 1. Compare only 2+ elements lists.
+        # 2. If an i-list is a sublist of a j-list and they are not the same lists we add +1 to a count variable.
+        # 3. If after checking all j-lists the count is 0 --> the algorithm adds i-list to the temp_sem_lists.
 
         temp_sem_lists = []
         for i in range(len(self.sem_lists)):
             count = 0
             for j in range(len(self.sem_lists)):
-                if self.sem_lists[i].lemmas.intersection(self.sem_lists[j].lemmas) == self.sem_lists[i].lemmas and i!=j:
+                if self.sem_lists[i].lemmas.intersection(self.sem_lists[j].lemmas) == self.sem_lists[i].lemmas and i != j:
                     count += 1
             if count == 0 and self.sem_lists[i].len > 0:
                 temp_sem_lists.append(self.sem_lists[i])
@@ -197,6 +355,7 @@ class SemanticList:
         nlp_doc (spaCy doc): A doc object which contains tokens from the essay
 
     """
+
     def __init__(self, hypersemant, sem_list, tokens):
         self.hypersemant = hypersemant
         self.list = sem_list
@@ -228,8 +387,3 @@ class SemanticList:
         count = Counter(tokens)
         count_of_occurrences = sum([count[token] for token in self.tokens])
         self.lex_div_characteristic = self.len / count_of_occurrences
-
-
-text = Text('wt2.txt')
-test = SemanticRelations(text)
-print(test.statistics())
