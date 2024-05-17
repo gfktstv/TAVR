6306db2f234e1c18decd5a10528a86b318dd792c
Grigorii Feoktistov <g.feoktistoff@gmail.com>
2024-05-17 23:05:20 +0700

    Main functionality added 1. Vocabulary chart added. 2. Tables for trigrams, academic formulas and indexes data added. 3. Replacement options by click on word can be seen in console log.
    Later some style will be added

--- web/connection_to_tavr.py
+++ web/connection_to_tavr.py
@@ -1,28 +1,69 @@
+import json
+
 from flask import Flask, request, jsonify, redirect, url_for, render_template
 from flask_cors import CORS
+
+import pandas as pd
+
 from tavr import *
 
 
-app = Flask(__name__)
-CORS(app)
-app.app_context().push()
+class Connection:
+    def __init__(self):
+        self._app = Flask(__name__)
+        CORS(self._app)
+        self._app.app_context().push()
+        self.tokens_text = list()
+        self.marked_up_tokens = dict()
+        self.token_id_dict = dict()
+
+    def connect(self):
+        @self._app.route('/get_tables', methods=['POST'])
+        def get_tables():
+            """Receives a data from the web page and returns tables from TAVR TextAnalysis"""
+            # Get the JSON data from the request and extracts input data (essay)
+            data = request.get_json()
+            essay = data.get('data')
+
+            tavr = TextAnalysis(essay)
+
+            trigrams, stats, academic_formulas = tavr.get_data_for_web()
+            trigrams_html = trigrams.to_html(index=False)
+            stats_html = stats.to_html(index=False, header=False)
+            academic_formulas_html = academic_formulas.to_html(index=False)
+
+            # Original dictionary from TAVR
+            self.marked_up_tokens = tavr.marked_up_tokens
+            # Dictionary token.text as a key and id as a value
+            for token in list(self.marked_up_tokens.keys()):
+                self.token_id_dict[f'{self.marked_up_tokens[token]['id']}'] = token
+            # List of tokens text to return it to javascript
+            self.tokens_text = [token.text for token in list(self.marked_up_tokens.keys())]
+            # Dictionary of tokens to return it to javascript
+            tokens_for_json = dict()
+            for key, value in tavr.marked_up_tokens.items():
+                tokens_for_json[f'{key.text}'] = value
+            with open('tokens.json', 'w') as f:
+                json.dump(tokens_for_json, f, indent=2, sort_keys=False)
+
+            return jsonify(trigrams_html, stats_html, academic_formulas_html)
+
+        @self._app.route('/get_tokens', methods=['GET'])
+        def get_tokens():
+            with open('tokens.json', 'r') as f:
+                tokens = json.load(f)
+            return jsonify(tokens, self.tokens_text)
+
+        @self._app.route('/get_replacements', methods=['POST'])
+        def get_replacements():
+            response = request.get_json()
+            id = response.get('data')
+            token = self.token_id_dict[f'{id}']
+            replacements = TokenReplacementOptions(self.marked_up_tokens).get_replacement_options(token, True)
+            return jsonify(replacements)
+
+        if __name__ == '__main__':
+            self._app.run(debug=True)
 
 
-@app.route('/')
-def index():
-    return redirect(url_for('static', filename='web/tavr-main.html'))
-
-
-@app.route('/receive_data', methods=['POST'])
-def receive_data():
-    """Receives a data from the web page"""
-    # Get the JSON data from the request and extracts input data (essay)
-    data = request.get_json()
-    essay = data.get('data')
-    measurements = LevelAndDescription(Text(essay)).description_dict
-
-    return jsonify(measurements)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+Connection().connect()
--- web/tavr-main-script.js
+++ web/tavr-main-script.js
@@ -1,5 +1,5 @@
 // Character counter
-document.getElementById('inputData').addEventListener('input', function() {
+document.getElementById('inputEssay').addEventListener('input', function() {
   var textareaValue = this.value.trim();
   var buttonAssess = document.getElementById('buttonAssess');
   var characterCounter = document.getElementById('characterCount')
@@ -15,61 +15,201 @@ document.getElementById('inputData').addEventListener('input', function() {
   characterCount.textContent = textareaValue.length;
 });
 
-// Send an essay to TAVR and get measurements
-function getMeasurementsFromTAVR(inputData) {
-  // Create a new XMLHttpRequest object
-  const xhr = new XMLHttpRequest();
-  // Define the endpoint URL of the Python server
-  const url = 'http://localhost:5000/receive_data';
-  // Set up the request
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  // Define a callback function to handle the response from the server
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-          if (xhr.status === 200) {
-              console.log('Data successfully sent to Python server.');
-              // Optionally handle the response from the server
-              console.log(xhr.responseText);
-          } else {
-              console.error('Failed to send data to Python server.');
-          }
-      }
-  };
-  // Convert JavaScript object to JSON string
-  const jsonData = JSON.stringify({ data: inputData });
-  // Send the request with the JSON data
-  xhr.send(jsonData);
-  // Get measurements from TAVR
-  measurements = xhr.responseText
-  return measurements
-}
-
-// Process essay and show assessment
+// Change the view of the web page after click on "Analyze"
 function showAssessment() {
-  // Sends essay to TAVR
-  const inputData = document.getElementById('inputData')
-  measurements = getMeasurementsFromTAVR(inputData.value)
-  console.log(measurements)
-  // Removes the buttonAssess and adds a buttonClear and a buttonCopy
-  const buttonAssess = document.getElementById('buttonAssess');
-  buttonAssess.remove();
-  const characterCount = document.getElementById('characterCount');
-  characterCount.remove();
-  const inputButton = document.getElementById('inputButton');
-  inputButton.innerHTML += `
-    <button id="buttonCopy" onclick="copyText()"><strong>Copy the essay</string></button>
-    <button id="buttonClear" onclick="clearText()"><strong>Clear the essay</strong></button>
-  `;
+  // Disable textarea
+  const textArea = document.getElementById('inputEssay');
+  textArea.disabled = true;
+  // Add measurements from TAVR
+  getMeasurementsFromTAVR(inputEssay);
 }
 
-// Change a view of the text after processing it
-function changeText() {
+async function getMeasurementsFromTAVR(inputEssay) {
+  // Change cursor view to show loading
+  document.body.style.cursor = 'wait'
 
+  const essay = inputEssay.value
+  const url = 'http://localhost:5000/get_tables';
+  try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ data: essay })
+      });
+      if (!response.ok) {
+          throw new Error('Failed to send data to Flask server');
+      }
+      // The response is 3 tables in json format
+      const tables = await response.json();
+      addMeasurements(tables['0'], tables['1'], tables['2']);
+      // The response is tokens in json format and tokens keys in array format (because in json tokens are sorted and idk why)
+      var tokens = await getTokens();
+      var markedTextArray = markUpText(tokens);
+      const inputMarkedEssay = document.getElementById('inputMarkedEssay');
+      inputMarkedEssay.id = 'markedEssay';
+      // Add marked up words
+      for (var i = 0; i < markedTextArray.length; i++) {
+        inputMarkedEssay.innerHTML += markedTextArray[i]
+        inputMarkedEssay.innerHTML += ` `
+      }
+      // Delete textarea
+      inputEssay.remove()
+      // Change title
+      document.getElementById('title-above-essay').innerHTML = 'the Tool for Analysis of Vocabulary Richness';
+      // Remove the buttonAssess and add a buttonClear and a buttonCopy
+      const buttonAssess = document.getElementById('buttonAssess');
+      buttonAssess.remove();
+      const characterCount = document.getElementById('characterCount');
+      characterCount.remove();
+      const inputButton = document.getElementById('inputButton');
+      inputButton.innerHTML += `
+        <button id="buttonCopy" onclick="copyText()"><strong>Copy</string></button>
+        <button id="buttonClear" onclick="clearText()"><strong>Clear</strong></button>
+      `;
+      // Make a smooth scroll a little bit lower
+      scrollTo({
+        top: 100,
+        behavior: "smooth"
+      })
+      // Change cursor view to default
+      document.body.style.cursor = 'default'
+  } catch (error) {
+      // Change cursor view to default
+      document.body.style.cursor = 'default'
+      alert('An error occurred, please, try again')
+      console.log(error)
+  }
+}
+
+async function getTokens() {
+  const url = 'http://localhost:5000/get_tokens';
+  try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (!response.ok) {
+        throw new Error('Failed to send data to Flask server');
+    }
+    const tokens = await response.json();
+    return tokens;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Add tables, chart and level to the web page
+function addMeasurements(tableTrigrams, tableStats, tableAcademicFormulas) {
+  const inputContainerAssessment = document.getElementById('inputContainerAssessment');
+  inputContainerAssessment.innerHTML += `
+    <div class='containerAssessment'>
+    <img class='vocabularyChart', src='vocabulary_chart.png'>
+    <div id='inputTableTrigrams'></div>
+    <div id='inputTableStats'></div>
+    <div id='inputTableAcademicFormulas'></div>
+    </div>
+  `;
+  const inputTableTrigrams = document.getElementById('inputTableTrigrams');
+  inputTableTrigrams.innerHTML = tableTrigrams;
+  const inputTableStats = document.getElementById('inputTableStats');
+  inputTableStats.innerHTML = tableStats;
+  const inputTableAcademicFormulas = document.getElementById('inputTableAcademicFormulas');
+  inputTableAcademicFormulas.innerHTML = tableAcademicFormulas;
+  const inputEssay = document.getElementById('inputEssay')
+  inputEssay.innerHTML = `<img class='vocabularyChart', src='vocabulary_chart.png'>`
+}
+
+function markUpText(tokens_and_keys) {
+  var tokens = tokens_and_keys['0'];
+  var keys = tokens_and_keys['1'];
+
+  // Create a new array to hold the marked up words
+  var markedTextArray = [];
+
+  // Iterate through each word
+  for (var i = 0; i < keys.length; i++) {
+      // Create a span element for each word
+      var span = document.createElement("span");
+      span.textContent = keys[i];
+      if (tokens[keys[i]]['stopword']) {
+        span.id = 'no_data'
+      } else {
+        span.id = idFromLevel(tokens[keys[i]]['level'])
+        span.setAttribute('onclick', 'getReplacements("' + tokens[keys[i]]['id'] + '")');
+      }
+      // Add the span element to the markedWords array
+      markedTextArray.push(span.outerHTML);
+  }
+
+  return markedTextArray
+}
+
+function idFromLevel(level) {
+  if (level === 'A1') {
+    result = 'level0'
+  } else if (level === 'A2') {
+    result = 'level1'
+  } else if (level === 'B1') {
+    result = 'level2'
+  } else if (level === 'B2') {
+    result = 'level3'
+  } else if (level === 'C1') {
+    reuslt = 'level4'
+  } else if (level === 'C2') {
+    result = 'level5'
+  } else {
+    result = 'no_data'
+  }
+  return result
+}
+
+function getHTMLFromString(str) {
+  // Create a new DOMParser
+  var parser = new DOMParser();
+
+  // Parse the string as XML
+  var xmlDoc = parser.parseFromString(str, "text/xml");
+
+  // Get the HTML representation of the parsed XML
+  var htmlString = xmlDoc.documentElement.innerHTML;
+
+  return htmlString;
+}
+
+async function getReplacements(id) {
+  // Change cursor view to show loading
+  document.body.style.cursor = 'wait'
+
+  const url = 'http://localhost:5000/get_replacements';
+  try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          }, 
+          body: JSON.stringify({ data: id })
+      });
+      if (!response.ok) {
+          throw new Error('Failed to send data to Flask server');
+      }
+      // The response is 3 tables in json format
+      const replacements = await response.json();
+      console.log(replacements)
+      // Change cursor view to default
+      document.body.style.cursor = 'default'
+  } catch (error) {
+      // Change cursor view to default
+      document.body.style.cursor = 'default'
+      console.log(error)
+      
+  }
 }
 
 // Buttons "clear" and "copy"
-
 function clearText() {
   if (confirm("Do you want to clear the essay and the stats?")) {
     location.reload()
@@ -77,8 +217,8 @@ function clearText() {
 }
 
 function copyText() {
-  const inputData = document.getElementById('inputData').value
-  navigator.clipboard.writeText(inputData).then(() => {
+  const inputEssay = document.getElementById('inputEssay').value
+  navigator.clipboard.writeText(inputEssay).then(() => {
     console.log('Content copied to clipboard');
     /* Resolved - text copied to clipboard successfully */
   },() => {
